125262d54207fe0b37e42d458d3841fab1c7efd1
phipix01 <philx_x@hotmail.com>
2015-12-21 16:47:58 +0100

    lots of improvements

--- ticTacToe/Program.cs
+++ ticTacToe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,18 @@ namespace ticTacToe
         const Byte CROSS = 2;
         const Byte CIRCLE = 4;
 
+        struct Position
+        {
+            public Byte X,Y;
+
+            public Position(Byte x, Byte y) : this()
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+        #region Helper Methods
         static void fill2dByteArray(Byte[,] array, Byte value)
         {
             for (int x = 0; x < array.GetLength(0); x++)
@@ -47,6 +60,27 @@ namespace ticTacToe
             }
         }
 
+        //Error Code 255 = key pressed is out of range. please enter keys from 0-2
+        static Byte getByteFromKey(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.D0:
+                case ConsoleKey.NumPad0:
+                    return 0;
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    return 1;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    return 2;
+                default:
+                    //#TODO: refactor: maybe there's a better solution.
+                    //return 255;
+                    throw new IndexOutOfRangeException("please enter keys from 0-2");
+            }
+        }
+
         static void drawInstructions()
         {
             Console.WriteLine("Welcome to ticTacToe");
@@ -56,37 +90,66 @@ namespace ticTacToe
             Console.Read();
         }
 
+        private static Position promptPosition(String player)
+        {
+            Console.WriteLine("{0} where do you want to place your sign?", player);
+            Console.Write(Environment.NewLine + "x:");
+            ConsoleKeyInfo x = Console.ReadKey();
+            Console.Write(Environment.NewLine + "y:");
+            ConsoleKeyInfo y = Console.ReadKey();
+            Byte enteredX = getByteFromKey(x.Key);
+            Byte enteredY = getByteFromKey(y.Key);
+            Console.WriteLine("{0} set a sign at {1}/{2}", player, enteredX, enteredY);
+            return new Position(enteredX, enteredY);
+        }
+        #endregion
 
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+
             Byte[,] grid = new Byte[3, 3];
             Boolean playerOnesTurn = true;
             Boolean noOneHasWonYet = true;
             drawInstructions();
-            
+            fill2dByteArray(grid, EMPTY);
+
             while (noOneHasWonYet)
             {
-                //Console.Clear();
+                Console.Clear();
+                draw2dByteArray(grid);
                 if (playerOnesTurn)
                 {
-                    Console.WriteLine("Player 1 please place a Cross");
-                    Console.Write(Environment.NewLine + "x:");
-                    ConsoleKeyInfo x = Console.ReadKey();
-                    Console.Write(Environment.NewLine + "y:");
-                    ConsoleKeyInfo y = Console.ReadKey();
-                    playerOnesTurn = !playerOnesTurn;
+                    
+                    Position signToAdd = promptPosition("player1");
+                    if (grid[signToAdd.X, signToAdd.Y] == EMPTY)
+                    {
+                        grid[signToAdd.X, signToAdd.Y] = CROSS;
+                        playerOnesTurn = !playerOnesTurn;
+                    }
+                    else
+                    {
+                        Console.WriteLine("[{0}/{1}] already contains {2}, please choose an empty field", signToAdd.X, signToAdd.Y, grid[signToAdd.X, signToAdd.Y]);
+                        Console.Read();
+                    }
                 }
                 else
                 {
-                    playerOnesTurn = !playerOnesTurn;
+                    Position signToAdd = promptPosition("player2");
+                    if (grid[signToAdd.X, signToAdd.Y] == EMPTY)
+                    {
+                        grid[signToAdd.X, signToAdd.Y] = CIRCLE;
+                        playerOnesTurn = !playerOnesTurn;
+                    }
+                    else
+                    {
+                        Console.WriteLine("[{0}/{1}] already contains {2}, please choose an empty field", signToAdd.X, signToAdd.Y, grid[signToAdd.X, signToAdd.Y]);
+                        Console.Read();
+                    }
                 }
             }
-            
+
             Console.Clear();
-            fill2dByteArray(grid, EMPTY);
-            grid[1, 1] = CROSS;
-            grid[2, 1] = CIRCLE;
-            draw2dByteArray(grid);
             Console.Read();
         }
     }
